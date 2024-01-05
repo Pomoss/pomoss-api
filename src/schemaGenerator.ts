d2d4a3447f5b8f76e85c788c677023f56b07a6cc
Takashi Kisaku <kisakutakashi0226@gmail.com>
2024-01-06 06:59:49 +0900

    Update depedencies and generate schema command

--- src/schemaGenerator.ts
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from 'fs';
-import { printSchema, lexicographicSortSchema } from 'graphql';
-import schema from '@/graphql/schema';
-
-const schemaAsString = printSchema(lexicographicSortSchema(schema));
-
-writeFileSync('schema.graphql', schemaAsString);
